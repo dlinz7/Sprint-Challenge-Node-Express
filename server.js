1ec907b85f2c4158fa2a1625717bfb8438496417
dlinz7 <linzdiegoa@gmail.com>
2018-06-05 18:29:11 -0500

    Latest update

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ server.get('/api/actions', (req, res) => {
     actions
     .get()
     .then(actions => {
-            res.json({actions});
+        res.json({actions});
     })
     .catch(error => {
         res.json({ message: "Info not found"})
@@ -29,31 +29,34 @@ server.get('/api/actions', (req, res) => {
 });
 
 server.post('/api/actions', (req, res) => {
-    const { description, notes} = req.body;
-    if(!description || !notes ) {
+    const { project_id, description} = req.body;
+    if(!description || !project_id ) {
         res.status(400);
-        res.json({ message: "Please provide description, notes "});
-        
+        res.json({ message: "Please provide description, project_id "});
+        return; 
     }
 
     actions 
 
-        .insert({ description, notes})
+        .insert({ project_id, description })
         .then(response => {
+            console.log(response);
             res.status(201);
-            res.json({ response });
+            res.json(response);
     })
         .catch(error => {
             console.log(error);
             res.status(500);
             res.json({message: "There is an error"});
-            return;
+            
     });
 
 
 });
 
-// get projects *works* 
+
+
+// GET projects *works* 
 server.get('/api/projects', (req, res) => {
     projects
     .get()
@@ -65,6 +68,50 @@ server.get('/api/projects', (req, res) => {
     })
 });
 
+//Project POST
+
+server.post('/api/projects', (req, res) => {
+    const { name, description} = req.body;
+    if(!description || !name ) {
+        res.status(400);
+        res.json({ message: "Please provide description, project_id "});
+        return; 
+    }
+    projects
+    .insert({name, description})
+    .then(projects => {
+        console.log(projects);
+        res.json(projects);
+        
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500);
+        res.json({error: "error"});
+    })
+})
+
+//Project DELETE
+
+server.delete('/api/projects/:id', (req, res) => {
+    const {id} = req.params;
+    projects.remove(id)
+        .then(response => {
+            if (response === 0) {
+                console.log(response);
+                res.status(404);
+                res.json({success: "id doesn't exist"});
+                return;
+        }
+        res.json({success: 'Item deleted'});
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(400);
+            res.json({error: 'Error'});
+        }
+        )
+})
 
 
 
